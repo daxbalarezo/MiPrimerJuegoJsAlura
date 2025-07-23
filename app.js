06b348259f772534cc98a23b3864020974c21cf9
Daniel Balarezo <dbalarezo15@gmail.com>
2025-07-23 16:20:13 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un numero porfavor:");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert("Acertaste el numero"); 
+}
